bd4c4834e1ac84f660684e4fbfa12731e4f19deb
Adrian Haarbach <mail@adrian-haarbach.de>
2016-01-02 10:40:55 +0100

    Theme.js replaced  qxex with qx for defaults

--- source/class/qxex/theme/Theme.js
+++ source/class/qxex/theme/Theme.js
@@ -10,12 +10,14 @@
 
 qx.Theme.define("qxex.theme.Theme",
 {
+  title : "qxex theme",
+
   meta :
   {
-    color : qxex.theme.Color,
+    color : qx.theme.modern.Color,
     decoration : qxex.theme.Decoration,
-    font : qxex.theme.Font,
-    icon : qx.theme.icon.Oxygen,
+    font : qx.theme.modern.Font,
+    icon : qx.theme.icon.Tango,
     appearance : qxex.theme.Appearance
   }
 });
