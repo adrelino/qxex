2b4f2e627af40bf6f4485e0558a29bf35457906f
Adrian Haarbach <mail@adrian-haarbach.de>
2016-01-06 10:52:02 +0200

    qxex.ui.form.DateField
    own DateField with invalid message when unable to parse

---
+++ source/class/qxex/ui/form/DateField.js
@@ -0,0 +1,56 @@
+/* ************************************************************************
+
+   Authors:
+     * Adrian Haarbach (adrelino)
+
+************************************************************************ */
+
+/**
+ * A *date field* with invalid states when we cant parse the date
+ */
+qx.Class.define("qxex.ui.form.DateField",
+{
+  extend : qx.ui.form.DateField,
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+
+    /*
+    ---------------------------------------------------------------------------
+      PUBLIC METHODS
+    ---------------------------------------------------------------------------
+    */
+
+
+    /**
+     * Returns the current set date, parsed from the input-field
+     * corresponding to the {@link #dateFormat}.
+     * If the given text could not be parsed, <code>null</code> will be returned.
+     *
+     * @return {Date} The currently set date.
+     */
+    getValue : function()
+    {
+      // get the value of the textfield
+      var textfieldValue = this.getChildControl("textfield").getValue();
+
+      // return the parsed date
+      try {
+        var val = this.getDateFormat().parse(textfieldValue);
+        this.setValid(true);
+        return val;
+      } catch (ex) {
+        this.setValid(false);
+        var formatter = this.getDateFormat();
+        this.setInvalidMessage(this.trc("tooltip","Invalid format. Correctly formatted example: %1",formatter.format(new Date())));
+        return null;
+      }
+    }
+  }
+});
