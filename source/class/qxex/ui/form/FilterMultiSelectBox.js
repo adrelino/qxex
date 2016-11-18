9e35798301640ca4ac3099369a50d99ca6024010
Adrian Haarbach <mail@adrian-haarbach.de>
2016-11-18 23:51:25 +0100

    add getChildByModel method

--- source/class/qxex/ui/form/FilterMultiSelectBox.js
+++ source/class/qxex/ui/form/FilterMultiSelectBox.js
@@ -1,11 +1,16 @@
 /**
  * MultiSelectBox with filter.
+ * 
+ * overrides add method and introduces getChildByModel method 
+ * that allows to retreive a child in constant time. TODO: move to mixin
  */
 qx.Class.define("qxex.ui.form.FilterMultiSelectBox", {
   extend : qxex.ui.form.MultiSelectBox,
   include : qxex.ui.form.MSelectBoxFilter,
 
   members : {
+    _childrenByModelHash : null,
+
     // overridden
     _onKeyPress : function(e)
     {
@@ -15,6 +20,20 @@ qx.Class.define("qxex.ui.form.FilterMultiSelectBox", {
       }else{
         this.base(arguments, e);
       }
+    },
+
+    //overridden from http://www.qooxdoo.org/current/apiviewer/#qx.ui.form.AbstractSelectBox
+    add : function(listItem, options){
+      this.base(arguments,listItem, options);
+      if(!this._childrenByModelHash) this._childrenByModelHash={};
+      this._childrenByModelHash[listItem.getModel()]=listItem;
+    },
+
+    /**
+     * Alternative to getChildren() and linear search if we have a model for the listItem
+     */
+    getChildByModel(model){
+      return this._childrenByModelHash ? this._childrenByModelHash[model] : null;
     }
   }
 });
