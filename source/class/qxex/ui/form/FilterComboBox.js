98668d815afaff4a6f634894f31a6fcd74c0233f
Adrian Haarbach <mail@adrian-haarbach.de>
2020-01-06 21:50:10 +0100

    FilterComboBox with label model replacement handling

--- source/class/qxex/ui/form/FilterComboBox.js
+++ source/class/qxex/ui/form/FilterComboBox.js
@@ -6,15 +6,48 @@ qx.Class.define("qxex.ui.form.FilterComboBox", { //TODO: search as you type
 
   construct : function(){
     this.base(arguments);
+    this._childrenByModelHash={};
+    this._childrenByLabelHash={};
   },
 
   members : {
+    _childrenByModelHash : null,
+    _childrenByLabelHash : null,
+
     hideInsteadOfDestroy : false,
 
     // overridden
     focus : function()
     {
       this.getChildControl("textfield").getFocusElement().focus();
+    },
+
+    //overridden from http://www.qooxdoo.org/current/apiviewer/#qx.ui.form.AbstractSelectBox
+    add : function(listItem, options){
+      this.base(arguments,listItem, options);
+      this._childrenByModelHash[listItem.getModel()]=listItem;
+      this._childrenByLabelHash[listItem.getLabel()]=listItem;
+    },
+
+    setModelValue : function(value)
+    {
+      console.log("setModelValue",value);
+      var item = this._childrenByModelHash[value];
+      if(item){
+        value = item.getLabel(); //key -> label replacement
+      }
+      this.setValue(value);
+    },
+
+    getModelValue : function()
+    {
+      var value = this.getValue();
+      var item = this._childrenByLabelHash[value];
+      if(item){
+        value = item.getModel(); //label -> key replacement
+      }
+      //console.log("getModelValue",value);
+      return value;
     },
 
     //overrridden
