aa6d5082c92010e2571480cc5194767d42b7002f
Adrian Haarbach <mail@adrian-haarbach.de>
2020-01-18 14:57:39 +0100

    MSelectBoxFilter with groupBox filter

--- source/class/qxex/ui/form/MSelectBoxFilter.js
+++ source/class/qxex/ui/form/MSelectBoxFilter.js
@@ -3,11 +3,31 @@
  */
 qx.Mixin.define("qxex.ui.form.MSelectBoxFilter", {
 
-  construct : function()
-  {
+  properties : {
 
+    displayFilterCheckBox :
+    {
+      check : "Boolean",
+      init : false,
+      apply : "_applyDisplayFilterCheckBox"
+    },
+
+    groupFilter :
+    {
+      check : "Function",
+      init  : function(item) {
+        return item.getUserData("filter").indexOf("47") > 0;
+      }
+    },
+
+    searchFilter :
+    {
+      check : "Function",
+      init  : function(item, filterTextLower) {
+        return item.getLabel().toLowerCase().indexOf(filterTextLower) > -1
+      }
+    }
   },
-
 
   members : { 
 
@@ -18,10 +38,7 @@ qx.Mixin.define("qxex.ui.form.MSelectBoxFilter", {
     var popup = this.getChildControl("popup");
 
     this.__filterTextField = textField || new qx.ui.form.TextField();
-    // input: The event is fired on every keystroke modifying the value of the field.
-    //this.__filterTextField.addListener("keyinput", this._onInput, this); // Does not help Firefox 66
-    //this.__filterTextField.addListener("input", this._onInput, this); // Does not help Firefox 66
-    //this.__filterTextField.addListener("changeValue", this._onInput, this); // Does not help Firefox 66
+    this.__filterTextField.addListener("changeValue", this._onFilterTextFieldChangeValue, this);
     
     // we fill the textfield by forwarding keyinputs and keypress (delete) to it so we dont have to give it focus,
     // which would interfere with the blur events
@@ -30,8 +47,8 @@ qx.Mixin.define("qxex.ui.form.MSelectBoxFilter", {
         this.addListener("keydown", this._onKeyDown, this);
         this.__filterTextField.setAnonymous(true);
         this.__filterTextField.setKeepFocus(true);
-    }else{
-        textField.addListener("input",this._onInputTextField, this);
+        var atom = this.getChildControl("atom");
+        //atom.setHeight(22);//atom.setMaxHeight(22);
     }
 
     this.__filterLabel = new qx.ui.basic.Label();
@@ -64,18 +81,42 @@ qx.Mixin.define("qxex.ui.form.MSelectBoxFilter", {
 
     },
 
+    addFilterCheckBox : function(){
+      var list = this.getChildrenContainer();
+      var popup = this.getChildControl("popup");
+      this.__filterCheckBox = new qx.ui.form.CheckBox(this.trc("checkbox","Group filter"));
+      this.__filterCheckBox.setKeepFocus(true);
+      this.__filterCheckBox.addListener("changeValue",this._onFilterCheckBoxChangeValue, this);
+      this.__filterCheckBox.setToolTipText(this.trc("tooltip","Check to filter out entries based on group filter"))
+      popup.addBefore(this.__filterCheckBox,list);
+    },
+
+    _onFilterCheckBoxChangeValue : function(val){
+      this.__filterList(this.__filterTextField.getValue() || "",val.getData());
+    },
+
+    _onFilterTextFieldChangeValue : function(val){
+      this.__filterList(val.getData() || "",this.__filterCheckBox && this.__filterCheckBox.getValue());
+    },
+
+    _applyDisplayFilterCheckBox : function(value, old){
+      if(value){
+        this.__filterCheckBox ? this.__filterCheckBox.show() : this.addFilterCheckBox();
+      }else{
+        this.__filterCheckBox && this.__filterCheckBox.exclude();
+      }
+    },
+
     //PUBLIC
     MIN_LIST_ITEMS_TO_SHOW_FILTER : 6, //we only display filter for 6 or more items
 
     setFilterText : function(filterText){
       this.MIN_LIST_ITEMS_TO_SHOW_FILTER = 0; //if we set programmatically, we want to be able to remove filter again even if we have less than 5 entries!
       this.__filterTextField.setValue(filterText);
-      this.__filterList(filterText);
     },
 
     clearFilter : function(){
       this.__filterTextField.setValue("");
-      this.__filterList("");
     },
 
     open : function(){
@@ -88,19 +129,21 @@ qx.Mixin.define("qxex.ui.form.MSelectBoxFilter", {
     },
 
     //PRIVATE
+    __filterCheckBox : null,
     __filterTextField : null,
     __filterLabel : null,
     __showFilter : false,
     __helpLabelEmpty : null,
 
-    __filterList : function(filterText){
-      if(!this.__showFilter) return;
+    __filterList : function(filterText, filterCheckBoxValue){
+      if(!this.__showFilter && !this.__filterCheckBox) return;
       var filterTextLower = filterText.toLowerCase();
       var count=0;
       var children = this.getChildren();
       children.forEach(function(item){
-          var show = item.getLabel().toLowerCase().indexOf(filterTextLower) > -1;
-          if(show){
+          var showText = filterTextLower.length == 0 || this.getSearchFilter()(item, filterTextLower);
+          var showFilter = (this.__filterCheckBox == null || filterCheckBoxValue == false) || this.getGroupFilter()(item);
+          if(showText && showFilter){
             item.show();
             count++;
           }
@@ -122,38 +165,25 @@ qx.Mixin.define("qxex.ui.form.MSelectBoxFilter", {
      */
     _onInput : function(e)
     {
+      if(!this.__showFilter) return;
       var old = this.__filterTextField.getValue() || "";
       var newVal = old+e.getChar();
       this.__filterTextField.setValue(newVal);
-      this.__filterList(newVal);
-      //forward to the filter:
-
-      //       var clone = e.clone();
-      //       clone.setTarget(this.__filterTextField);
-      //       clone.setBubbles(false);
-      //       this.__filterTextField.dispatchEvent(clone);
     },
 
     // overridden
     _onKeyDown : function(e)
     {
+      if(!this.__showFilter) return;
       var iden = e.getKeyIdentifier();
       if(iden=="Backspace"){
         var old = this.__filterTextField.getValue() || "";
         var newVal = old.slice(0, -1);
         this.__filterTextField.setValue(newVal);
-        this.__filterList(newVal);
 
         e.preventDefault();
         return;
       }
-    },
-
-    // overridden
-    _onInputTextField : function(e)
-    {
-        var newVal = this.__filterTextField.getValue() || "";
-        this.__filterList(newVal);
     }
   }
 });
