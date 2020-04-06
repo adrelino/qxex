e879372b4e69671138438a0d3ce5e196db6da930
Adrian Haarbach <mail@adrian-haarbach.de>
2020-04-06 12:23:18 +0200

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ DIR=${1%/} #strips trailing slashes
 #echo "This script $0 is called with $# arguments, namely $@"
 if [ $# -ne 1 ]
 then
-	echo "Usage: $0 {folder}"
+	echo "Usage: $0 {folder|file}"
 	exit 1
 fi
 
@@ -12,15 +12,15 @@ fi
 #git rm -rf .
 if [ -e "$DIR" ] #-e file or dir, -f file, -d dir
 then
-	echo "---- deploying folder $DIR to gh-pages----";
+	echo "---- deploying $DIR to gh-pages----";
 	git add $DIR -f;
 	git stash;
 	git checkout gh-pages;
-	git checkout stash@{0} -- $DIR/;  #TODO: deletes unsaved changes in master
-	git commit -m "deploying folder $DIR";
+	git checkout stash@{0} -- $DIR;  #TODO: deletes unsaved changes in master
+	git commit -m "deploying $DIR";
 	git push origin gh-pages; #only push on gh-pages branch, not on master
 	git checkout master; #TODO: deletes unsaved changes in master
 	echo '---- switched back to master ----';
 else
-	echo "folder $DIR does not exist"
+	echo "folder|file $DIR does not exist"
 fi
