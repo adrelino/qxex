48573309d91820716ef72f19b296ecc0afb56fe2
Adrian Haarbach <mail@adrian-haarbach.de>
2021-03-10 14:54:44 +0100

    re-enabled CH added date-holidays and region-flags for IS+NO and European micro-states.

--- source/resource/cpdr.sh
+++ source/resource/cpdr.sh
@@ -1,4 +1,4 @@
 #call as ./cpdr.sh ~/date-holidays/ ~/region-flags/
 echo "$1 $2"
 cp -t "./qxex/date-holidays/dist" "$1dist/umd.min.js" 
-while read -r line; do for file in $line; do cp "$2pngWx12px/$file.png" "qxex/region-flags/pngWx12px"; done; done < $1regions-and-subregions.txt
+while read -r line; do for file in $line; do echo $file; cp -n "$2pngWx12px/$file.png" "qxex/region-flags/pngWx12px"; done; done < $1regions-and-subregions.txt
