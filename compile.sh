5d40e71268777efa88c6528756937ee03c1dac3b
Adrian Haarbach <mail@adrian-haarbach.de>
2021-03-22 21:24:56 +0100

    bootPath

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #https://qooxdoo.org/documentation/6.0/#/development/cli/commands?id=building-for-production-and-deployment
-npx qx compile --target=build
+npx qx compile --target=build #--clean
 npx qx deploy --out=./build #--source-maps
