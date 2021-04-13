a1679e40ffdb68fbe70d1281beec7ece845ac013
Adrian Haarbach <mail@adrian-haarbach.de>
2021-04-13 20:10:27 +0200

    translation migration
    npx qx compile --update-po-files

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #https://qooxdoo.org/documentation/6.0/#/development/cli/commands?id=building-for-production-and-deployment
-npx qx compile --target=build #--clean
+#npx qx compile --update-po-files #translation
+#npx qx compile --target=build #--clean
 npx qx deploy --out=./build #--source-maps
